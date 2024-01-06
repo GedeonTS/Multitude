ca455a851b962fd53cf69d7fb46429a4599b515c
GedeonTS <gedeontshobohwa1@gmail.com>
2024-01-07 00:21:47 +0200

    Add author name to the blog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,8 +23,8 @@
         <div style="background: grey">
             <h2> All Posts</h2>
             @foreach($posts as $post)
-                <h3>{{$post->title}}</h3>
-                <p>{{$post->body}}</p>P
+                <h3>{{$post->title}} by {{$post->user['name']}}</h3>
+                <p>{{$post->body}}</p>
                 <p><a href="edit-post/{{$post->id}}">Edit</a></p>
                 <form action="/delete-post/{{$post->id}}" method="POST">
                     @csrf
