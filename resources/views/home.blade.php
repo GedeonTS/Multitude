60407f0386820454f4faa38e2ede28a6439a3f79
GedeonTS <gedeontshobohwa1@gmail.com>
2024-01-06 12:47:03 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    @auth 
+        <p>Hi, Congrats your are logged in.</p>
+        <form action="/logout" method="POST">
+            @csrf
+            <button>Logout</button>
+        </form>
+    @else
+    <div style="border: 3px solid black">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+    @csrf
+    <input type="text" name="name" placeholder="name">
+    <input type="text" name="email" placeholder="email">
+    <input type="password" name="password" placeholder="password">
+    <button>Register</button>
+</form>
+</div>
+<div style="border: 3px solid black">
+    <h2>Login</h2>
+    <form action="/login" method="POST">
+    @csrf
+    <input type="text" name="loginname" placeholder="name">
+    <input type="password" name="loginpassword" placeholder="password">
+    <button>Log in</button>
+</form>
+</div>
+@endauth
+</body>
+</html>
